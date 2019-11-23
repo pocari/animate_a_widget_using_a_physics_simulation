6d7c7239e2d3e34d81eb67075fe87fb5089cda0a
pocari <caffelattenonsugar@gmail.com>
2019-11-23 15:23:16 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/animate_a_widget_using_a_physics_simulation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animate_a_widget_using_a_physics_simulation
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
